2f0c5e94f845a7921a77e8126ebb8041ca21ff19
Alex Pariona <lx7pary@gmail.com>
2016-09-09 16:25:30 -0500

    update types

--- model/jpa/src/main/java/org/openfact/models/jpa/entities/ublType/TextType.java
+++ model/jpa/src/main/java/org/openfact/models/jpa/entities/ublType/TextType.java
@@ -2,6 +2,7 @@ package org.openfact.models.jpa.entities.ublType;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 /**
@@ -9,5 +10,25 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 @Access(AccessType.FIELD)
-public class TextType extends TextTypeUnqDat {
+public class TextType {
+	@Column(name = "VALUE")
+	protected String value;
+	@Column(name = "LANGUAGE_ID")
+	protected String languageID;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getLanguageID() {
+		return languageID;
+	}
+
+	public void setLanguageID(String languageID) {
+		this.languageID = languageID;
+	}
 }
