a656c66cf559d07fa56c49db51fefdd1216ac05a
Alex Pariona <lx7pary@gmail.com>
2016-09-26 10:34:42 -0500

    ..

--- services/src/main/java/org/openfact/ubl/send/pe/UblPESenderProvider.java
+++ services/src/main/java/org/openfact/ubl/send/pe/UblPESenderProvider.java
@@ -1,5 +1,12 @@
 package org.openfact.ubl.send.pe;
 
+import java.io.FileOutputStream;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.xml.ws.soap.SOAPFaultException;
+
 import org.openfact.models.OpenfactSession;
 import org.openfact.models.OrganizationModel;
 import org.openfact.models.ubl.CreditNoteModel;
@@ -8,6 +15,13 @@ import org.openfact.models.ubl.InvoiceModel;
 import org.openfact.services.ServicesLogger;
 import org.openfact.ubl.send.UblSenderException;
 import org.openfact.ubl.send.UblSenderProvider;
+import org.openfact.ubl.send.pe.header.UblHeaderHandlerResolver;
+import org.openfact.ubl.send.pe.sunat.BillService;
+import org.openfact.ubl.send.pe.sunat.BillService_Service;
+import org.w3c.dom.Document;
+
+import com.helger.ubl21.UBL21Writer;
+import com.sun.xml.ws.util.ByteArrayDataSource;
 
 public class UblPESenderProvider implements UblSenderProvider {
 
@@ -26,28 +40,52 @@ public class UblPESenderProvider implements UblSenderProvider {
 	}
 
 	@Override
-	public void send(OrganizationModel orgamization, InvoiceModel invoice, byte[] document, String name)
-			throws UblSenderException {
+	public void send(OrganizationModel organization, InvoiceModel invoice, byte[] document, String contentType,
+			String name) throws UblSenderException {
+		try {
+			// Call Web Service Operation
+			BillService_Service service = new BillService_Service();
+			service.setHandlerResolver(new UblHeaderHandlerResolver());
+			BillService port = service.getBillServicePort();
+
+			// TODO initialize WS operation arguments here
+			DataSource dataSource = new ByteArrayDataSource(document, contentType);
+			DataHandler contentFile = new DataHandler(dataSource);
+
+			// DataSource fds = new
+			// FileDataSource("/home/lxpary/Documents/10428482072-01-F001-1.zip");
+			// DataHandler contentFile = new DataHandler(fds);
+
+			// TODO process result here
+			byte[] result = port.sendBill(name, contentFile);
+
+			System.out.println("Result = " + result);
+		} catch (SOAPFaultException ex) {
+			System.out.println(ex.getFault().getFaultCode());
+			System.out.println(ex.getFault().getFaultString());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void send(OrganizationModel organization, DebitNoteModel debitNote, byte[] document, String contentType,
+			String name) throws UblSenderException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void send(OrganizationModel orgamization, DebitNoteModel debitNote, byte[] document, String name)
-			throws UblSenderException {
+	public void send(OrganizationModel organization, CreditNoteModel creditNote, byte[] document, String contentType,
+			String name) throws UblSenderException {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void send(OrganizationModel orgamization, CreditNoteModel creditNote, byte[] document, String name)
+	public void send(OrganizationModel organization, byte[] document, String contentType, String name)
 			throws UblSenderException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void send(OrganizationModel orgamization, byte[] document, String name) throws UblSenderException {
 		// TODO Auto-generated method stub
 
 	}
