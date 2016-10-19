28bca7d5ee1d8d6c9482f181f2c84c816e49a606
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-10-19 15:53:19 -0500

    task runner

--- services/src/main/java/org/openfact/ubl/send/pe/UblTaskRunnerProvider_PE.java
+++ services/src/main/java/org/openfact/ubl/send/pe/UblTaskRunnerProvider_PE.java
@@ -1,7 +1,17 @@
 package org.openfact.ubl.send.pe;
 
+import java.io.File;
+
+import javax.sql.DataSource;
+
+import org.easybatch.core.job.Job;
+import org.easybatch.core.job.JobBuilder;
+import org.easybatch.core.job.JobExecutor;
+import org.easybatch.core.job.JobReport;
 import org.openfact.models.OpenfactSession;
 import org.openfact.ubl.send.UblTaskRunnerProvider;
+
+import static org.easybatch.core.job.JobBuilder.aNewJob;
 
 public class UblTaskRunnerProvider_PE implements UblTaskRunnerProvider {
 
@@ -24,7 +34,12 @@ public class UblTaskRunnerProvider_PE implements UblTaskRunnerProvider {
     }
 
     protected void sendInvoices() {
+        //Job job = JobBuilder.aNewJob().named("myName").build();
 
+        // Execute the job
+        /*JobExecutor jobExecutor = new JobExecutor();
+        JobReport jobReport = jobExecutor.execute(job);
+        jobExecutor.shutdown();*/
     }
 
     protected void sendCreditNotes() {
