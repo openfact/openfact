abebaf09f1264dcf4cdbfc11222f5cbb4d3c3a86
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-03-10 19:01:05 -0500

    changes basic

--- testsuite/integration/src/main/java/org/openfact/Main.java
+++ testsuite/integration/src/main/java/org/openfact/Main.java
@@ -1,37 +1,34 @@
 package org.openfact;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.openfact.models.MyProvider;
 import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.datasources.DatasourceArchive;
 import org.wildfly.swarm.datasources.DatasourcesFraction;
-import org.wildfly.swarm.jaxrs.JAXRSArchive;
+import org.wildfly.swarm.spi.api.JARArchive;
 import org.wildfly.swarm.undertow.WARArchive;
 
 public class Main {
 
-    public static void main(String... args) throws Exception {
-        // Instantiate the container
-        Swarm swarm = new Swarm().fraction(new DatasourcesFraction()
-                .jdbcDriver("h2", (d) -> {
-                    d.driverClassName("org.h2.Driver");
-                    d.xaDatasourceClass("org.h2.jdbcx.JdbcDataSource");
-                    d.driverModuleName("com.h2database.h2");
-                })
-                .dataSource("ExampleDS", (ds) -> {
-                    ds.driverName("h2");
-                    ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-                    ds.userName("sa");
-                    ds.password("sa");
-                }));
-
-        // Create one or more deployments
-        //WARArchive deployment = ShrinkWrap.create(WARArchive.class);
-
-
-        // Add resource to deployment
-        //deployment.addClass(MyProvider.class);
-
+    public static void main(String[] args) throws Exception {
+        Swarm swarm = new Swarm();
         swarm.start();
-        swarm.deploy(/*deployment*/);
+
+        // Create a JDBC driver deployment using maven groupId:artifactId
+        // The version is resolved from your pom.xml's <dependency>
+        swarm.deploy(Swarm.artifact("com.h2database:h2", "h2"));
+
+        // Create a DS deployment
+        JARArchive dsArchive = ShrinkWrap.create(JARArchive.class);
+        dsArchive.as(DatasourceArchive.class).dataSource("ExampleDS", (ds) -> {
+            ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+            ds.driverName("h2");
+            ds.userName("sa");
+            ds.password("sa");
+        });
+
+        swarm.deploy(dsArchive);
+
+        WARArchive appDeployment = ShrinkWrap.create(WARArchive.class).addAllDependencies();
+        swarm.deploy(appDeployment);
     }
 }
