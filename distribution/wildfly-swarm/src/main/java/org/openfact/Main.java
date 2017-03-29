e309e26092f0fdfd1dfa1b3a385d4bac5064f173
carlosthe19916 <carlosthe19916@gmail.com>
2017-03-29 00:06:17 -0500

    changes

--- distribution/wildfly-swarm/src/main/java/org/openfact/Main.java
+++ distribution/wildfly-swarm/src/main/java/org/openfact/Main.java
@@ -1,9 +1,12 @@
 package org.openfact;
 
+import java.net.URL;
+
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.datasources.DatasourcesFraction;
+import org.wildfly.swarm.keycloak.Secured;
 import org.wildfly.swarm.undertow.WARArchive;
 
 public class Main {
@@ -11,7 +14,10 @@ public class Main {
     static String driverModule;
 
     public static void main(String[] args) throws Exception {
-        Swarm swarm = new Swarm();
+        ClassLoader cl = Main.class.getClassLoader();
+        URL stageConfig = cl.getResource("project-defaults.yml");
+
+        Swarm swarm = new Swarm(false).withConfig(stageConfig);
 
         String userDB = System.getProperty("swarm.user.db", "h2");
 
@@ -34,12 +40,16 @@ public class Main {
                 driverModule = "com.h2database.h2";
         }
 
-
         swarm.start();
 
         WARArchive appDeployment = ShrinkWrap.create(WARArchive.class);
-        appDeployment.addPackage("org.openfact")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+
+        appDeployment.as(Secured.class)
+                .protect("/*")
+                .withMethod("GET", "POST", "PUST", "DELETE")
+                .withRole("admin", "organization-manager", "organization-user");
+
+        appDeployment.addPackage("org.openfact").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
                 .addAllDependencies();
 
         // Deploy your app
@@ -47,48 +57,42 @@ public class Main {
     }
 
     private static DatasourcesFraction datasourceWithH2() {
-        return new DatasourcesFraction()
-                .jdbcDriver("h2", (d) -> {
-                    d.driverClassName("org.h2.Driver");
-                    d.xaDatasourceClass("org.h2.jdbcx.JdbcDataSource");
-                    d.driverModuleName("com.h2database.h2");
-                })
-                .dataSource("ExampleDS", (ds) -> {
-                    ds.driverName("h2");
-                    ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-                    ds.userName("sa");
-                    ds.password("sa");
-                });
+        return new DatasourcesFraction().jdbcDriver("h2", (d) -> {
+            d.driverClassName("org.h2.Driver");
+            d.xaDatasourceClass("org.h2.jdbcx.JdbcDataSource");
+            d.driverModuleName("com.h2database.h2");
+        }).dataSource("ExampleDS", (ds) -> {
+            ds.driverName("h2");
+            ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+            ds.userName("sa");
+            ds.password("sa");
+        });
     }
 
     private static DatasourcesFraction datasourceWithPostgresql() {
-        return new DatasourcesFraction()
-                .jdbcDriver("org.postgresql", (d) -> {
-                    d.driverClassName("org.postgresql.Driver");
-                    d.xaDatasourceClass("org.postgresql.xa.PGXADataSource");
-                    d.driverModuleName("org.postgresql");
-                })
-                .dataSource("ExampleDS", (ds) -> {
-                    ds.driverName("org.postgresql");
-                    ds.connectionUrl("jdbc:postgresql://localhost:5432/postgres");
-                    ds.userName("postgres");
-                    ds.password("postgres");
-                });
+        return new DatasourcesFraction().jdbcDriver("org.postgresql", (d) -> {
+            d.driverClassName("org.postgresql.Driver");
+            d.xaDatasourceClass("org.postgresql.xa.PGXADataSource");
+            d.driverModuleName("org.postgresql");
+        }).dataSource("ExampleDS", (ds) -> {
+            ds.driverName("org.postgresql");
+            ds.connectionUrl("jdbc:postgresql://localhost:5432/postgres");
+            ds.userName("postgres");
+            ds.password("postgres");
+        });
     }
 
     private static DatasourcesFraction datasourceWithMysql() {
-        return new DatasourcesFraction()
-                .jdbcDriver("com.mysql", (d) -> {
-                    d.driverClassName("com.mysql.jdbc.Driver");
-                    d.xaDatasourceClass("com.mysql.jdbc.jdbc2.optional.MysqlXADataSource");
-                    d.driverModuleName("com.mysql");
-                })
-                .dataSource("ExampleDS", (ds) -> {
-                    ds.driverName("com.mysql");
-                    ds.connectionUrl("jdbc:mysql://localhost:3306/mysql");
-                    ds.userName("root");
-                    ds.password("root");
-                });
+        return new DatasourcesFraction().jdbcDriver("com.mysql", (d) -> {
+            d.driverClassName("com.mysql.jdbc.Driver");
+            d.xaDatasourceClass("com.mysql.jdbc.jdbc2.optional.MysqlXADataSource");
+            d.driverModuleName("com.mysql");
+        }).dataSource("ExampleDS", (ds) -> {
+            ds.driverName("com.mysql");
+            ds.connectionUrl("jdbc:mysql://localhost:3306/mysql");
+            ds.userName("root");
+            ds.password("root");
+        });
     }
 
 }
