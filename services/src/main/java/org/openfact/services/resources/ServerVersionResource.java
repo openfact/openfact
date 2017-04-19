b3fc27c71af058a2375655e6467a877badfa7aca
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-04-19 15:43:04 -0500

    remove unnecessary resources

--- services/src/main/java/org/openfact/services/resources/ServerVersionResource.java
+++ services/src/main/java/org/openfact/services/resources/ServerVersionResource.java
@@ -1,29 +1,17 @@
 package org.openfact.services.resources;
 
-import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.NoCache;
-import org.jboss.resteasy.spi.HttpRequest;
-import org.jboss.resteasy.spi.HttpResponse;
 import org.openfact.representations.VersionRepresentation;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Stateless
 @Path("/version")
 public class ServerVersionResource {
-
-    protected static final Logger logger = Logger.getLogger(ServerVersionResource.class);
-
-    @Context
-    protected HttpRequest request;
-
-    @Context
-    protected HttpResponse response;
 
     @GET
     @NoCache
