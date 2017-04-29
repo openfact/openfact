24e52f36d4d46ae23c539b206635cdb048d3cff4
carlos <carlosthe19916@gmail.com>
2017-04-28 23:01:49 -0500

    travis

--- .travis/gpg.sh
+++ .travis/gpg.sh
@@ -12,8 +12,8 @@ cat >gen-key-script <<EOF
     Key-Length: 4096
     Subkey-Type: 1
     Subkey-Length: 4096
-    Name-Real: Opensource Idealo
-    Name-Email: opensource-logback-redis@idealo.de
+    Name-Real: Openfact
+    Name-Email: openfact@openfact.com
     Expire-Date: 2y
     Passphrase: ${GPG_PASSPHRASE}
     %commit
