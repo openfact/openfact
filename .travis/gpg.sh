af6aee2b123df33221258b8d7795007d40ba2838
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-05-02 13:01:45 -0500

    1.0.RC13 SNAPSHOT

--- .travis/gpg.sh
+++ .travis/gpg.sh
@@ -13,7 +13,7 @@ cat >gen-key-script <<EOF
       Subkey-Type: 1
       Subkey-Length: 4096
       Name-Real: Openfact
-      Name-Email: carlosthe19916@sistcoop.com
+      Name-Email: openfact@openfact.com
       Expire-Date: 2y
       Passphrase: ${GPG_PASSPHRASE}
       %commit
@@ -21,7 +21,7 @@ cat >gen-key-script <<EOF
 EOF
 
 # create a local keypair with given configuration
-$GPG_EXECUTABLE --batch --gen-key gen-key-script
+gpg --batch --gen-key gen-key-script
 
 
 # export created GPG key
@@ -31,16 +31,16 @@ $GPG_EXECUTABLE --batch --gen-key gen-key-script
 # uid                  Lars K.W. Gohlke <lars.gohlke@idealo.de>
 # ssb   4096R/CC1613B2 2016-09-08
 # ssb   4096R/55B7CAA2 2016-09-08
-export GPG_KEYNAME=$($GPG_EXECUTABLE -K | grep ^sec | cut -d/  -f2 | cut -d\  -f1 | head -n1)
+export GPG_KEYNAME=$(gpg -K | grep ^sec | cut -d/  -f2 | cut -d\  -f1 | head -n1)
 
 # cleanup local configuration
 shred gen-key-script
 
 # publish the gpg key  (sonatype reads from keyserver.ubuntu.com)
-$GPG_EXECUTABLE --keyserver keyserver.ubuntu.com --send-keys ${GPG_KEYNAME}
+gpg --keyserver keyserver.ubuntu.com --send-keys ${GPG_KEYNAME}
 
 # wait for the key beeing accessible
 while(true); do
     date
-    $GPG_EXECUTABLE --keyserver keyserver.ubuntu.com --recv-keys ${GPG_KEYNAME} && break || sleep 30
+    gpg --keyserver keyserver.ubuntu.com --recv-keys ${GPG_KEYNAME} && break || sleep 30
 done
