0a4e72b881ac6959171a2f2f0a90402df36d16c9
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-05-02 12:22:17 -0500

    changes

--- .travis/gpg.sh
+++ .travis/gpg.sh
@@ -5,7 +5,7 @@ set -e
 # create a random passphrase
 export GPG_PASSPHRASE=$(echo "$RANDOM$(date)" | md5sum | cut -d\  -f1)
 
-# configuration to generate gpg2 keys
+# configuration to generate gpg keys
 cat >gen-key-script <<EOF
       %echo Generating a basic OpenPGP key
       Key-Type: RSA
@@ -21,7 +21,7 @@ cat >gen-key-script <<EOF
 EOF
 
 # create a local keypair with given configuration
-gpg2 --batch --gen-key gen-key-script
+$GPG_EXECUTABLE --batch --gen-key gen-key-script
 
 
 # export created GPG key
@@ -31,16 +31,16 @@ gpg2 --batch --gen-key gen-key-script
 # uid                  Lars K.W. Gohlke <lars.gohlke@idealo.de>
 # ssb   4096R/CC1613B2 2016-09-08
 # ssb   4096R/55B7CAA2 2016-09-08
-export GPG_KEYNAME=$(gpg2 -K | grep ^sec | cut -d/  -f2 | cut -d\  -f1 | head -n1)
+export GPG_KEYNAME=$($GPG_EXECUTABLE -K | grep ^sec | cut -d/  -f2 | cut -d\  -f1 | head -n1)
 
 # cleanup local configuration
 shred gen-key-script
 
-# publish the gpg2 key  (sonatype reads from keyserver.ubuntu.com)
-gpg2 --keyserver keyserver.ubuntu.com --send-keys ${GPG_KEYNAME}
+# publish the gpg key  (sonatype reads from keyserver.ubuntu.com)
+$GPG_EXECUTABLE --keyserver keyserver.ubuntu.com --send-keys ${GPG_KEYNAME}
 
 # wait for the key beeing accessible
 while(true); do
     date
-    gpg2 --keyserver keyserver.ubuntu.com --recv-keys ${GPG_KEYNAME} && break || sleep 30
+    $GPG_EXECUTABLE --keyserver keyserver.ubuntu.com --recv-keys ${GPG_KEYNAME} && break || sleep 30
 done
