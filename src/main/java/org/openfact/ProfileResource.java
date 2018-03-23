1de69852efcb336965a27e310d8e2e0157e53adc
Carlos Feria <carlosthe19916@gmail.com>
2018-03-23 17:10:40 -0500

    feat(template): add keycloak

--- src/main/java/org/openfact/ProfileResource.java
+++ src/main/java/org/openfact/ProfileResource.java
@@ -8,6 +8,7 @@ import org.openfact.security.ISecurityContext;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,6 +16,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 
+@Transactional
 @Path("profile")
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
