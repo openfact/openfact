2985547e4dc772d42f5b3f6618b33d8294d1307a
Carlos Feria <carlosthe19916@gmail.com>
2018-05-11 08:40:53 -0500

    change method of publish to maven

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -3,3 +3,8 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     openssl aes-256-cbc -K $encrypted_eedb18d92bf8_key -iv $encrypted_eedb18d92bf8_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
+
+if [[ "${TRAVIS_TAG}" != "" ]]; then
+    openssl aes-256-cbc -K $encrypted_eedb18d92bf8_key -iv $encrypted_eedb18d92bf8_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
+fi
