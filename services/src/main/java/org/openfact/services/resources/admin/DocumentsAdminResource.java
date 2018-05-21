0bd047844f8bb9bf3168fc65ede168c8d25da1d1
Carlos Feria <carlosthe19916@gmail.com>
2018-05-21 09:18:28 -0500

    fix(add default filter): if no operator set EQ as default operator

--- services/src/main/java/org/openfact/services/resources/admin/DocumentsAdminResource.java
+++ services/src/main/java/org/openfact/services/resources/admin/DocumentsAdminResource.java
@@ -527,7 +527,7 @@ public class DocumentsAdminResource {
                         throw new BadRequestException("Bad operator on criteria");
                     }
                 } else {
-                    query.addFilter(filter.getName(), filter.getValue(), filter.getOperator());
+                    query.addFilter(filter.getName(), filter.getValue(), filter.getOperator() != null ?  filter.getOperator() : SearchCriteriaFilterOperator.eq);
                 }
             }
         }
